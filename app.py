170154eddbdee541a6376b62e8c8b14391b0f9fd
Konrad Gądek <kgadek@gmail.com>
2014-06-09 15:11:01 +0200

    integration experiments

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import pickle
 from sys import argv
+import itertools
 
 import os
 from os import environ as env
 import bottle
-from bottle import request, response, get, post, HTTPResponse
+from bottle import request, response, get, post
 import requests
-import itertools
 
 
 bottle.debug(True)
@@ -40,41 +40,37 @@ def index():
     return ret
 
 
-@post('/example_post/<doc_id>')
-def example_post(doc_id):
-    # yes, this is a sec–hole :) if you're bored: do whatever you like!
-    doc_file = os.path.join("tmp", doc_id)
-
-    with open(doc_file, "w") as fh:
-        fh.write("=====try A=====")
-        try:
-            content = request.forms.get('content')[0:100]
-            fh.write("=====A read=====")
-            fh.write(content)
-            fh.write("=====A done=====")
-        except Exception, e:
-            fh.write("=====A fail=====\n" + e.message + '\n\n')
-
-        fh.write("=====try B=====")
-        try:
-            content = request.json
-            fh.write("=====B read=====")
-            content = str(content)
-            fh.write("=====B str=====")
-            fh.write(content)
-            fh.write("=====B done=====")
-        except Exception, e:
-            fh.write("=====B fail=====\n" + e.message + '\n\n')
+@post('/push')
+def pushed_ztis_data():
+    old_data = []
+    # noinspection PyBroadException
+    try:
+        with open("res.txt", "rb") as fh:
+            old_data = pickle.load(fh)
+    except:
+        pass
+    data = request.json
+    types = get_types(data)
+    res = get_sequences(types, data, THRESHOLD)
+    old_data.append(res)
+    with open("res.txt", "wb") as fh:
+        pickle.dump(old_data, fh)
 
 
-@get('/example_get/<doc_id>')
-def example_get(doc_id):
-    # yes, this is a sec–hole :) if you're bored: do whatever you like!
-    doc_file = os.path.join("tmp", doc_id)
-    with open(doc_file, "r") as fh:
-        return fh.read()
-    # noinspection PyUnreachableCode
-    return HTTPResponse("No such file!", status=400)
+@get('/results')
+def results():
+    old_data = []
+    try:
+        with open("res.txt", "rb") as fh:
+            old_data = pickle.load(fh)
+    except:
+        pass
+    return str(old_data)
+
+
+
+
+
 
 
 def get_data(number_of_data, timespan):
@@ -89,7 +85,9 @@ def get_segment(timespan):
     get_request = requests.get("http://immense-refuge-2812.herokuapp.com/sample/learn?config=1")
     if get_request.status_code != 200:
         raise Exception()
+    print "DEBUG", "get_request.json() =", get_request.json()
     data = get_request.json()['series']
+    print "DEBUG", "data =", data
     return [x['type'] for x in data]
 
 
@@ -147,8 +145,9 @@ def get_sequences(types, data, threshold):
 
 bottle.run(host='0.0.0.0', port=argv[1])
 # data = get_data(NUMBER_OF_DATA, TIMESPAN)
-# print(data)
+# print "GETDATA"
+# print data
 # types = get_types(data)
-##print(types)
-#
-# print(get_sequences(types, data, THRESHOLD))
+# #print(types)
+# print "get_sequences"
+# print get_sequences(types, data, THRESHOLD)
